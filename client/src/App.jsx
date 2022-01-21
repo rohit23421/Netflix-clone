3193eb1b3274189bf1481fddca43dec4adf9ffb4
Rohit sah <61084486+rohit23421@users.noreply.github.com>
2022-01-21 18:15:29 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Home />
+      {/* <Watch />
+      <Register />
+      <Login /> */}
+    </div>
+  );
+};
+
+export default App;
