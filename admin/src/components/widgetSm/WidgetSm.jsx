d878e528b470adbaf91f66cd7894388bcae45c7b
Rohit sah <61084486+rohit23421@users.noreply.github.com>
2022-01-23 23:26:45 +0530

    Add files via upload

---
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,51 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWFkYjg2ZDllOGEwYmM1ZDZkNmU4MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Mjg3NjQwNiwiZXhwIjoxNjQzMzA4NDA2fQ.RR5L7RKRmj5jMvr4ECYHSeYDJ3mog7pdXtlk_Un54d0",
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUsers();
+  }, []);
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {newUsers.map((user) => (
+          <li className="widgetSmListItem">
+            <img
+              src={
+                user.profilePic ||
+                "https://pbs.twimg.com/media/D8tCa48VsAA4lxn.jpg"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
